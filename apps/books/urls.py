7b26cab912155d255cd3d7bc33a22ca0fb6ee62c
hengage <tunecarterkillz@gmail.com>
2022-08-20 07:12:58 +0100

    Feature: book retrieve and update endpoint

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import BookListCreateAPIView
+from .views import BookListCreateAPIView, BookRetrieveUpdateAPIView
 
 urlpatterns = [
-    path('', BookListCreateAPIView.as_view(), name='books')
+    path('', BookListCreateAPIView.as_view(), name='books'),
+    path('<int:pk>/', BookRetrieveUpdateAPIView.as_view(), name='book')
 ]
--- apps/books/views.py
+++ apps/books/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 
 from .models import BookModel
 from .serializers import BookSerializer
 
 class BookListCreateAPIView(ListCreateAPIView):
+    queryset = BookModel.objects.all()
+    serializer_class = BookSerializer
+
+class BookRetrieveUpdateAPIView(RetrieveUpdateAPIView):
     queryset = BookModel.objects.all()
     serializer_class = BookSerializer
