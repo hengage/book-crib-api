bffe9688f2effe466b415c7b2a531a613a30db8f
hengage <tunecarterkillz@gmail.com>
2022-08-20 03:49:13 +0100

    Feature: register author in admin area

--- apps/authors/admin.py
+++ apps/authors/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from django.contrib.auth import get_user_model
+Author = get_user_model()
+
+@admin.register(Author)
+class CustomUserAdmin(UserAdmin):
+    model = Author
+    list_display = ['email', 'first_name', 'last_name']
+
+    ordering = ["first_name"]
+
+    fieldsets= [
+        ("Basic User Details", {
+            "fields": ['email', 'first_name', 'last_name',]
+        }),
+
+        (
+            "User Status (sensitive area, proceed with caution)",
+            {
+                "fields": [
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ],
+                "classes": ["collapse"],
+            },
+        ),
+
+        (
+            None,
+            {
+                "fields": [
+                    "last_login",
+                ]
+            },
+        ),
+
+        (None, {"fields": ["groups", "user_permissions"]}),
+    ]
+
+    add_fieldsets = (
+        (
+            "User Personal Details",
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password",
+                ),
+            },
+        ),
+
+        (
+            "User status",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_superuser",
+                    "is_active"
+                ),
+            },
+        ),
+    )
