a04c8573dcc75b80986f09a1b96dfeb10de8ec37
hengage <tunecarterkillz@gmail.com>
2022-08-20 03:37:42 +0100

    Fixed bug: fixed write only fields, set to password

--- apps/authors/serializers.py
+++ apps/authors/serializers.py
@@ -10,7 +10,7 @@ class AuthorSerializer(serializers.ModelSerializer):
         fields = [
             'email', 'first_name', 'last_name', 'password'
         ]
-        extra_kwargs = {"passowrd": {"write_only": True}}
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
