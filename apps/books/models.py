f83ce4888fe5c3495381597b2c15cc751e1a389a
hengage <tunecarterkillz@gmail.com>
2022-08-20 05:59:56 +0100

    feature: create book model

--- apps/books/models.py
+++ apps/books/models.py
@@ -1,3 +1,17 @@
+from tabnanny import verbose
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class BookModel(models.Model):
+    title = models.CharField(max_length=150, blank=False, null=False)
+    isbn = models.PositiveIntegerField(unique=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author'
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = ['Book']
+        verbose_name_plural = ['Books']
